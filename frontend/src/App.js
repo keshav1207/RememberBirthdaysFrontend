72029008040224b486e31d16a220c3bad5ab04f2
Keshav Callychurn <keshav0799@gmail.com>
2025-06-25 18:04:53 -0400

    Recreate form using react hook form and implement validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,23 @@
-import React from "react";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 export default function App() {
-  const [formValues, setFormValues] = useState({
-    firstName: "",
-    lastName: "",
-    birthDate: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
-
-  async function recordBirthday(e) {
-    console.log(formValues);
+  async function recordBirthday(values) {
+    console.log(values);
     try {
-      e.preventDefault();
-
-      setFormValues({
-        firstName: "",
-        lastName: "",
-        birthDate: "",
-      });
-
       const response = await axios.post("http://localhost:8080/api/people", {
-        firstName: formValues.firstName,
-        lastName: formValues.lastName,
-        birthDate: formValues.date,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        birthDate: values.date,
       });
+
+      reset();
 
       console.log(response);
     } catch (error) {
@@ -41,31 +27,27 @@ export default function App() {
 
   return (
     <div>
-      <form onSubmit={recordBirthday}>
+      <form onSubmit={handleSubmit(recordBirthday)}>
         <h1> Birthday </h1>
         <label>First Name: </label>
         <input
-          name="firstName"
-          type="text"
-          value={formValues.firstName}
-          onChange={handleChange}
+          {...register("firstName", { required: "First name is required" })}
         />
-
+        {errors.firstName && <p>{errors.firstName.message}</p>}
         <label>Last Name: </label>
         <input
-          name="lastName"
-          type="text"
-          value={formValues.lastName}
-          onChange={handleChange}
+          {...register("lastName", { required: "Last name is required" })}
         />
-
+        {errors.lastName && <p>{errors.lastName.message}</p>}
         <input
-          name="date"
           type="date"
-          value={formValues.date}
-          onChange={handleChange}
+          {...register("birthDate", {
+            required: "Birth date is required",
+            validate: (value) =>
+              new Date(value) < new Date() || "Date must be in the past",
+          })}
         />
-
+        {errors.birthDate && <p>{errors.birthDate.message}</p>}
         <button type="submit">Submit</button>
       </form>
     </div>
