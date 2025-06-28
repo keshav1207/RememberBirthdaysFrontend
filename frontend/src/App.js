b3dd964903a1048a5289881c9354c0850600b731
Keshav Callychurn <keshav0799@gmail.com>
2025-06-28 15:44:39 -0400

    Set up react router to have multiple pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,14 @@
-import "./App.css";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
-  async function recordBirthday(values) {
-    console.log(values);
-    try {
-      const response = await axios.post("http://localhost:8080/api/people", {
-        firstName: values.firstName,
-        lastName: values.lastName,
-        birthDate: values.date,
-      });
-
-      reset();
-
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
+function App() {
   return (
-    <div>
-      <form className="addBirthdayForm" onSubmit={handleSubmit(recordBirthday)}>
-        <h1> Birthday </h1>
-        <label>First Name: </label>
-        <input
-          {...register("firstName", { required: "First name is required" })}
-        />
-        {errors.firstName && <p>{errors.firstName.message}</p>}
-        <label>Last Name: </label>
-        <input
-          {...register("lastName", { required: "Last name is required" })}
-        />
-        {errors.lastName && <p>{errors.lastName.message}</p>}
-        <label>Birthday Date: </label>
-        <input
-          type="date"
-          {...register("birthDate", {
-            required: "Birth date is required",
-            validate: (value) =>
-              new Date(value) < new Date() || "Date must be in the past",
-          })}
-        />
-        {errors.birthDate && <p>{errors.birthDate.message}</p>}
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,57 @@
+import "../App.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  async function recordBirthday(values) {
+    console.log(values);
+    try {
+      const response = await axios.post("http://localhost:8080/api/people", {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        birthDate: values.date,
+      });
+
+      reset();
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <form className="addBirthdayForm" onSubmit={handleSubmit(recordBirthday)}>
+        <h1> Birthday </h1>
+        <label>First Name: </label>
+        <input
+          {...register("firstName", { required: "First name is required" })}
+        />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
+        <label>Last Name: </label>
+        <input
+          {...register("lastName", { required: "Last name is required" })}
+        />
+        {errors.lastName && <p>{errors.lastName.message}</p>}
+        <label>Birthday Date: </label>
+        <input
+          type="date"
+          {...register("birthDate", {
+            required: "Birth date is required",
+            validate: (value) =>
+              new Date(value) < new Date() || "Date must be in the past",
+          })}
+        />
+        {errors.birthDate && <p>{errors.birthDate.message}</p>}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
