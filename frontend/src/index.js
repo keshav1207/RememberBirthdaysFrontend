90e9cd0da84c81a9295219039398ffcc72872f15
Keshav Callychurn <keshav0799@gmail.com>
2025-09-25 21:26:50 -0400

    Add redirect to Keycloak login when react frontend starts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { authConfig } from "./services/authConfig";
+import { AuthProvider } from "react-oauth2-code-pkce";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider authConfig={authConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend/src/services/authConfig.js
@@ -0,0 +1,10 @@
+export const authConfig = {
+  clientId: "RememberBirthdays-PKCE",
+  authorizationEndpoint:
+    "http://127.0.0.1:8080/realms/RememberBirthdays/protocol/openid-connect/auth",
+  tokenEndpoint:
+    "http://127.0.0.1:8080/realms/RememberBirthdays/protocol/openid-connect/token",
+  redirectUri: "http://localhost:3000",
+  scope: "openid profile email offline_access",
+  onRefreshTokenExpire: (event) => event.logIn(),
+};
