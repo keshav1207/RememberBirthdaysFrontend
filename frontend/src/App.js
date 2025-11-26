bc7cbecee133c900c46d45c0a0a46d1bef004eec
Keshav Callychurn <keshav0799@gmail.com>
2025-11-25 21:03:45 -0500

    Implement protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import AddBirthday from "./pages/addBirthday";
 import AllBirthday from "./pages/allBirthday";
 import UserInfo from "./pages/userInfo.jsx";
 import Admin from "./pages/admin.jsx";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
 
 function App() {
+  const { tokenData } = useContext(AuthContext);
+  const isAdmin = tokenData?.realm_access?.roles?.includes("Admin");
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AllBirthday />} />
-        <Route path="/addBirthday" element={<AddBirthday />} />
-        <Route path="/userInfo" element={<UserInfo />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route
+          path="/addBirthday"
+          element={tokenData ? <AddBirthday /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/userInfo"
+          element={tokenData ? <UserInfo /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/admin"
+          element={isAdmin ? <Admin /> : <Navigate to="/" />}
+        />
       </Routes>
     </Router>
   );
