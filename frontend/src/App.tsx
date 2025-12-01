fef419e1770aec8ab311d86a47846a6954a5d14a
Keshav Callychurn <keshav0799@gmail.com>
2025-11-30 19:42:39 -0500

    Recode app.tsx to remove infinite loop bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+
 import AddBirthday from "./pages/addBirthday";
 import AllBirthday from "./pages/allBirthday";
 import UserInfo from "./pages/userInfo";
 import Admin from "./pages/admin";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "react-oauth2-code-pkce";
 
 function App() {
-  const { tokenData, logIn } = useContext(AuthContext);
-  const [redirecting, setRedirecting] = useState(true);
-  const [loginStarted, setLoginStarted] = useState(false);
+  const { tokenData, loginInProgress, logIn, error } = useContext(AuthContext);
+  const [initialized, setInitialized] = useState(false);
   const isAdmin = tokenData?.realm_access?.roles?.includes("Admin");
 
   useEffect(() => {
-    if (!tokenData && !loginStarted) {
-      setLoginStarted(true);
+    if (!loginInProgress && !tokenData) {
       logIn();
     } else if (tokenData) {
-      setRedirecting(false);
+      setInitialized(true);
     }
-  }, [tokenData, logIn, loginStarted]);
+  }, [loginInProgress, tokenData, logIn]);
 
-  if (redirecting) {
-    return <div>Loading...</div>;
+  if (!initialized) {
+    return <div></div>;
   }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AllBirthday />} />
-        <Route
-          path="/addBirthday"
-          element={tokenData ? <AddBirthday /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/userInfo"
-          element={tokenData ? <UserInfo /> : <Navigate to="/" />}
-        />
+        element={isAdmin ? <Admin /> : <Navigate to="/allBirthday" />}
+        <Route path="/allBirthday" element={<AllBirthday />} />
+        <Route path="/addBirthday" element={<AddBirthday />} />
+        <Route path="/userInfo" element={<UserInfo />} />
         <Route
           path="/admin"
-          element={isAdmin ? <Admin /> : <Navigate to="/" />}
+          element={isAdmin ? <Admin /> : <Navigate to="/allBirthday" />}
         />
       </Routes>
     </Router>
