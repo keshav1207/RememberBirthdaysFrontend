0d211af471967c3b9c1f30a638f9c02c35674c73
Keshav Callychurn <keshav0799@gmail.com>
2025-12-15 20:04:09 -0500

    Change local endpoint  to dockerized ones

--- frontend/src/services/authConfig.tsx
+++ frontend/src/services/authConfig.tsx
@@ -5,9 +5,9 @@ interface RefreshTokenExpireEvent {
 export const authConfig = {
   clientId: "RememberBirthdays-PKCE",
   authorizationEndpoint:
-    "http://127.0.0.1:8080/realms/RememberBirthdays/protocol/openid-connect/auth",
+    "http://keycloak:8080/realms/RememberBirthdays/protocol/openid-connect/auth",
   tokenEndpoint:
-    "http://127.0.0.1:8080/realms/RememberBirthdays/protocol/openid-connect/token",
+    "http://keycloak:8080/realms/RememberBirthdays/protocol/openid-connect/token",
   redirectUri: "http://localhost:3000",
   scope: "openid profile email offline_access",
   onRefreshTokenExpire: (event: RefreshTokenExpireEvent) => event.logIn(),
