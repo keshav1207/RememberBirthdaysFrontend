72ea92bd6e7752da8ebb4df0e5ae60e0ce3dedc0
keshav <keshav0799@gmail.com>
2026-05-24 18:08:41 -0400

    Add keycloak realm and client id as env variable for reusability

--- frontend/src/services/authConfig.tsx
+++ frontend/src/services/authConfig.tsx
@@ -7,9 +7,9 @@ const KEYCLOAK_BASE_URL = process.env.REACT_APP_KEYCLOAK_BASE_URL || "http://loc
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || "http://localhost:3000";
 
 export const authConfig = {
-  clientId: "RememberBirthdays-PKCE",
-  authorizationEndpoint: `${KEYCLOAK_BASE_URL}/realms/RememberBirthdays/protocol/openid-connect/auth`,
-  tokenEndpoint: `${KEYCLOAK_BASE_URL}/realms/RememberBirthdays/protocol/openid-connect/token`,
+  clientId:  process.env.REACT_APP_KEYCLOAK_CLIENT_ID || "RememberBirthdays-PKCE",
+  authorizationEndpoint: `${KEYCLOAK_BASE_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/auth`,
+  tokenEndpoint: `${KEYCLOAK_BASE_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`,
   redirectUri: REDIRECT_URI,
   scope: "openid profile email offline_access",
   onRefreshTokenExpire: (event: RefreshTokenExpireEvent) => event.logIn(),
