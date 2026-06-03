9194eb9d81e950eaf34857edcc217d4727596a19
keshav <keshav0799@gmail.com>
2026-06-03 19:51:40 -0400

    Add more matches to jest through the setUpTests.ts file

---
+++ frontend/src/setUpTests.ts
@@ -0,0 +1,7 @@
+// Runs automatically before every test file.
+// Imports jest-dom which adds extra DOM matchers to Jest
+// e.g. toBeInTheDocument(), toHaveValue(), toBeDisabled()
+// This means these matchers are available in all test files without needing to import them individually.
+
+
+import '@testing-library/jest-dom';
